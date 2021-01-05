7e0d4291322a151c258de304b8d1ade16249486e
HziOng <hzl15717965853@163.com>
2021-01-05 18:49:45 +0800

    第二次提交，更新了概要计划、2021.1.5日总结和当日为了学习netty创建的模块demo：study

---
+++ sutdy/src/main/java/org/hzl/aknife/discard/handler/DiscardClientHandler.java
@@ -0,0 +1,44 @@
+package org.hzl.aknife.discard.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.Scanner;
+
+public class DiscardClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final ByteBuf firstMessage;
+    Scanner input = new Scanner(System.in);
+
+    public DiscardClientHandler() {
+        firstMessage = Unpooled.buffer(DiscardClient.SIZE);
+        for (int i = 0; i < firstMessage.capacity(); i ++) {
+            firstMessage.writeByte((byte) i);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstMessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
